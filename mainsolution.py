0311c6f6ad05af3a098c8e93e4b186a055bf8cee
valeriejingxn <s10242412@connect.np.sg>
2022-07-29 12:20:15 +0800

    test

--- mainsolution.py
+++ mainsolution.py
@@ -1,1 +1,6 @@
-
+import requests
+api_key = "JU5Q2GN41LOPWZ76"
+url= f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=({api_key}'
+response = requests.get(url)
+data = response.json()
+print(data.keys())
